a365aff61ae20cfa3d42c98931e0ff3f057ab299
Gavin <Gavinszczesniak@gmail.com>
2022-02-09 14:38:23 -0500

    Lab we were assigned to do that was about arrays and array methods. Will be adding the extra challenges soon.

---
+++ script.js
@@ -0,0 +1,93 @@
+"use strict";
+
+const submissions = [
+  {
+    name: "Jane",
+    score: 95,
+    date: "2020-01-24",
+    passed: true,
+  },
+  {
+    name: "Joe",
+    score: 77,
+    date: "2018-05-14",
+    passed: true,
+  },
+  {
+    name: "Jack",
+    score: 59,
+    date: "2019-07-05",
+    passed: false,
+  },
+  {
+    name: "Jill",
+    score: 88,
+    date: "2020-04-22",
+    passed: true,
+  },
+];
+
+const addSubmission = (array, newName, newScore, newDate) => {
+  array.push({
+    name: newName,
+    score: newScore,
+    date: newDate,
+    passed: newScore >= 60,
+  });
+};
+
+addSubmission(submissions, "Evan", 86, "2022-02-09");
+
+const deleteSubmissionByIndex = (array, index) => array.splice(index, 1);
+deleteSubmissionByIndex(submissions, 1);
+
+const deleteSubmissionByName = (array, name) => {
+  const indexName = array.findIndex((item) => item.name === name);
+  array.splice(indexName, 1);
+};
+deleteSubmissionByName(submissions, "Jill");
+
+const editSubmission = (array, index, score) => {
+  array[index].score = score;
+  array[index].passed = score >= 60;
+};
+
+editSubmission(submissions, 1, 54);
+console.log(submissions);
+
+const findSubmissionsByName = (array, name) =>
+  array.find((item) => item.name == name);
+
+console.log(findSubmissionsByName(submissions, "Jane"));
+
+const findLowestScore = (array) => {
+  let lowestScore = array[0].score;
+  array.forEach((person) => {
+    if (person.score < lowestScore) {
+      lowestScore = person;
+    }
+  });
+  return lowestScore;
+};
+
+console.log(findLowestScore(submissions));
+
+const findAverageScore = (array) => {
+  let total = 0;
+  for (let score of array) {
+    total += score.score;
+  }
+  return total / array.length;
+};
+console.log(findAverageScore(submissions));
+
+const filterPassing = (array) => {
+  return array.filter((item) => item.passed);
+};
+console.log(filterPassing(submissions));
+
+const filter90AndAbove = (array) => {
+  return array.filter((item) => item.score >= 90);
+};
+
+console.log(filter90AndAbove(submissions));
